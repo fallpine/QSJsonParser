572eaaaf8ef17dc2a80c26809d48aacbf8b9147c
Song <>
2026-04-15 14:44:56 +0800

    1.0.1（2）

---
+++ QSJsonParser/QSJsonParserWatch Watch App/QSJsonParserWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  QSJsonParserWatchApp.swift
+//  QSJsonParserWatch Watch App
+//
+//  Created by JXH_2 on 2026/4/15.
+//
+
+import SwiftUI
+
+@main
+struct QSJsonParserWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
